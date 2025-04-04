424b929a87836f3a02ba55badc752cfe3d45f59f
rayane-araujo <rayanearaujo28@hotmail.com>
2025-04-03 21:49:11 -0300

    first commit

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components"
+
+const MyGlobalStyles = createGlobalStyle`
+
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body{
+        background-color: #F3F3F3;
+    }
+
+`
+
+export default  MyGlobalStyles
